e060247e1013b30084e9c1dab91ce9bb6080ee29
Jaakko Hannikainen <jaakko.hannikainen@paivola.fi>
2015-04-14 12:06:31 +0300

    fix tests

--- src/test/java/com/github/superryhma/miniprojekti/dao/impl/ReferenceDAOInMemoryImplTest.java
+++ src/test/java/com/github/superryhma/miniprojekti/dao/impl/ReferenceDAOInMemoryImplTest.java
@@ -1,8 +1,14 @@
 package com.github.superryhma.miniprojekti.dao.impl;
 
 import com.github.superryhma.miniprojekti.dao.ReferenceDAO;
+import com.github.superryhma.miniprojekti.models.Attribute;
+import com.github.superryhma.miniprojekti.models.Reference;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,5 +24,12 @@ public class ReferenceDAOInMemoryImplTest {
     @Test
     public void emptyDatabaseReturnsEmpty() {
         assertEquals(dao.getReferences().size(), 0);
+    }
+
+    @Test
+    public void canAddReferenceToDatabase() {
+        Reference ref = new Reference("article", "test", Date.from(Instant.now()), null, new HashSet<>(), new HashSet<>());
+        dao.addReference(ref);
+        assertEquals(dao.getReferences().size(), 1);
     }
 }
