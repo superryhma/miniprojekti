96e329ff73f9901b43ae0ca80739072e3b127d65
Tomi <tpheiska@saunalahti.fi>
2015-04-14 09:44:38 +0300

    korjaus V1__create_tables.sql:ään

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,47 +1,47 @@
-CREATE TABLE USER{
+CREATE TABLE USER(
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL,
     digestive TEXT NOT NULL
-};
+);
  
-CREATE TABLE Project{
+CREATE TABLE Project(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     owner INTEGER NOT NULL REFERENCES USER(id)
-};
+);
  
-CREATE TABLE Reference_type{
+CREATE TABLE Reference_type(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
-};
+);
  
-CREATE TABLE Attribute_type{
+CREATE TABLE Attribute_type(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
-};
+);
  
-CREATE TABLE Dependency{
+CREATE TABLE Dependency(
     reference_type INTEGER PRIMARY KEY REFERENCES Reference_type(id),
     attribute_type INTEGER PRIMARY KEY REFERENCES Attribute_type(id),
     required BOOLEAN NOT NULL DEFAULT FALSE
-};
+);
  
-CREATE TABLE Reference{
+CREATE TABLE Reference(
     id SERIAL PRIMARY KEY,
     reference_type INTEGER NOT NULL REFERENCES Reference_type(id),
     project INTEGER NOT NULL REFERENCES Project(id),
     bibtextname TEXT NOT NULL
-};
+);
  
-CREATE TABLE Attribute{
+CREATE TABLE Attribute(
     id SERIAL PRIMARY KEY,
     reference INTEGER NOT NULL REFERENCES Reference(id),
     attribute_type INTEGER NOT NULL REFERENCES Attribute_type(id),
     value TEXT NOT NULL
-};
+);
  
-CREATE TABLE Tag{
+CREATE TABLE Tag(
     id SERIAL PRIMARY KEY,
     reference INTEGER REFERENCES Reference(id),
     value TEXT NOT NULL
-};
+);
