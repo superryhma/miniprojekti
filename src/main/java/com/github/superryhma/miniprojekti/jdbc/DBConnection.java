7739dff2228a07d9a92e5dba404324262267b0ba
Jaakko Hannikainen <jgke@jgke.fi>
2015-04-15 17:51:58 +0300

    update DBConnection to the newest version

--- src/main/java/com/github/superryhma/miniprojekti/jdbc/DBConnection.java
+++ src/main/java/com/github/superryhma/miniprojekti/jdbc/DBConnection.java
@@ -4,30 +4,52 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.postgresql.ds.PGPoolingDataSource;
+
 public class DBConnection {
-	private final InitialContext cxt;
-	private final DataSource dataSource;
+    private InitialContext cxt;
+    private DataSource dataSource;
 
-	public DBConnection() throws NamingException {
-		this.cxt = new InitialContext();
-        this.dataSource = (DataSource) cxt.lookup("java:comp/env/jdbc/DataSource");
-	}
+    public DBConnection() {
+    }
 
-	public Connection getConnection() throws SQLException {
-		return dataSource.getConnection();
-	}
+    public Connection getConnection() throws SQLException {
+        try {
+            this.cxt = new InitialContext();
+            this.dataSource = (DataSource) cxt.lookup("DataSource");
+            return dataSource.getConnection();
+        } catch (NamingException e) {
+            PGPoolingDataSource source = new PGPoolingDataSource();
+            source.setDataSourceName("A Data Source");
+            source.setServerName("localhost");
+            source.setPortNumber(5432);
+            source.setDatabaseName("miniprojekti");
+            source.setUser("miniprojekti");
+            source.setPassword("secret");
+            source.setMaxConnections(10);
+            try {
+                new InitialContext().rebind("DataSource", source);
+                this.dataSource = (DataSource) cxt.lookup("DataSource");
+                return dataSource.getConnection();
+            } catch (NamingException e1) {
+                e1.printStackTrace();
+            }
+        }
+        return null;
+    }
 
-	public Statement getStatement() throws SQLException {
-		Statement st = this.getConnection().createStatement();
-		return st;
-	}
+    public Statement getStatement() throws SQLException {
+        Statement st = this.getConnection().createStatement();
+        return st;
+    }
 
-	public PreparedStatement getPreparedStatement(String sql)
-			throws SQLException {
-		return this.getConnection().prepareStatement(sql);
-	}
+    public PreparedStatement getPreparedStatement(String sql)
+            throws SQLException {
+        return this.getConnection().prepareStatement(sql);
+    }
 }
