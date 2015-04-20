a1b7937e4a908a5dc924f56be714e3752affb10c
Aku Silvennoinen <augustus58@gmail.com>
2015-04-20 17:23:22 +0300

    bug fixed in ReferenceDAODBImpl delete

--- src/test/easyb/add_inproceedings_Story.groovy
+++ src/test/easyb/add_inproceedings_Story.groovy
@@ -1,1 +1,7 @@
 description 'User can add inproceedings references'
+
+scenario "user adds inproceedings with correct fields", {
+    given 'POST to /api/reference/ arrives'
+    when 'data in json are correct inproceedings data'
+    then 'reference is added to db correctly'
+}
