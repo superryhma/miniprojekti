382fb4e6e1bb14b0eaf2d6ce79fb90be45eb39a6
Markus Mustonen <makesmi@hotmail.com>
2015-04-21 10:00:00 +0300

    konflikti

--- src/test/easyb/add_article_Story.groovy
+++ src/test/easyb/add_article_Story.groovy
@@ -1,1 +1,16 @@
 description 'User can add article references'
+
+
+scenario 'User can add article with correct fields', {
+    given '', {
+        post('/api/references/')
+    }
+    
+    when '', {
+        
+    }
+    
+    then '', {
+        
+    }
+}
