35be530fd9fce50490a28c5bddcb50e2b9237902
Markus Mustonen <makesmi@hotmail.com>
2015-04-23 19:01:01 +0300

    ReferenceTypeDAODBImpl refaktorointi

--- src/main/java/com/github/superryhma/miniprojekti/dao/impl/db/ReferenceTypeDAODBImpl.java
+++ src/main/java/com/github/superryhma/miniprojekti/dao/impl/db/ReferenceTypeDAODBImpl.java
@@ -9,6 +9,7 @@ import com.github.superryhma.miniprojekti.dao.impl.db.models.AttributeType;
 import com.github.superryhma.miniprojekti.dao.impl.db.models.ReferenceTypeDb;
 import com.github.superryhma.miniprojekti.dbc.Dbc;
 import com.github.superryhma.miniprojekti.models.ReferenceType;
+import java.util.stream.Collectors;
 
 public class ReferenceTypeDAODBImpl implements ReferenceTypeDAO {
 
@@ -25,27 +26,26 @@ public class ReferenceTypeDAODBImpl implements ReferenceTypeDAO {
     @Override
     public Set<ReferenceType> getTypes() {
         Dbc.open();
-        List<ReferenceTypeDb> typesList = ReferenceTypeDb.findAll();
-        Set<ReferenceType> typesSet = new HashSet<>();
-        String name;
-        for (ReferenceTypeDb type : typesList) {
-            name = (String) type.get("name");
-            typesSet.add(new ReferenceType(name, getFields(name, true),
-                    getFields(name, false)));
-        }
+        Set<ReferenceType> typesSet = 
+                ReferenceTypeDb.findAll().stream()
+                .map(type -> (String)type.get("name"))
+                .map(this::loadReferenceType)
+                .collect(Collectors.toSet());
         Dbc.close();
         return typesSet;
+    }
+    
+    private ReferenceType loadReferenceType(String name){
+        return new ReferenceType(name, getFields(name, true), getFields(name, false));
     }
 
     private Set<String> getFields(String type, boolean required) {
         Dbc.open();
-        ReferenceTypeDb rt = ReferenceTypeDb.findFirst("name = ?", type);
-        List<AttributeType> typesList = rt.get(AttributeType.class,
-                "required = ?", required);
-        Set<String> typesSet = new HashSet<String>();
-        for (AttributeType t : typesList) {
-            typesSet.add(t.getString("name"));
-        }
+        Set<String> typesSet = 
+                ReferenceTypeDb.findFirst("name = ?", type)
+                .get(AttributeType.class, "required = ?", required).stream()
+                .map(t -> t.getString("name"))
+                .collect(Collectors.toSet());
         Dbc.close();
         return typesSet;
     }
